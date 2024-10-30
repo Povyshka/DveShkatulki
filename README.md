a74d3829100fc1957951a64820e218ac4acd459c
Povysheva Ekaterina <povyshka@gmail.com>
2024-10-30 12:05:46 +0300

    fix mobile styles and edit readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DveShkatulki
+
+Три правильно угаданные буквы дают вам право на две шкатулки...
+
+Выбирай с умом, ведь попытка всего лишь одна!
+
+![Dve Shkatulki](./client/public/dve.gif)
+
+https://dve-shkatulki.povysh.ru/
+
+---
+Спасибо за помощь, [бро](https://github.com/agospher4eg)!
--- client/src/App/Components/Footer/Footer.jsx
+++ client/src/App/Components/Footer/Footer.jsx
@@ -13,15 +13,20 @@ export default function Footer() {
             href="https://github.com/Povyshka"
             rel="noreferrer"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
               <path
-                fill="black"
+                fill="#ae6dd5"
                 d="M5 2h4v2H7v2H5V2Zm0 10H3V6h2v6Zm2 2H5v-2h2v2Zm2 2v-2H7v2H3v-2H1v2h2v2h4v4h2v-4h2v-2H9Zm0 0v2H7v-2h2Zm6-12v2H9V4h6Zm4 2h-2V4h-2V2h4v4Zm0 6V6h2v6h-2Zm-2 2v-2h2v2h-2Zm-2 2v-2h2v2h-2Zm0 2h-2v-2h2v2Zm0 0h2v4h-2v-4Z"
               />
             </svg>
           </a>
           <a href="https://t.me/povyshka" target="_blank" rel="noreferrer">
-            <img src='/icons/tg.png' alt='tg-icon'/>
+            <img src="/icons/tg.png" alt="tg-icon" />
           </a>
         </div>
       </div>
